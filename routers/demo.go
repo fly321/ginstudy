a1bb5a4f1be5195055d6abea7d2e7e560385bb56
fly3210 <15267840622@163.com>
2022-08-21 23:38:25 +0800

    路由分组配置中间件

--- routers/demo.go
+++ routers/demo.go
@@ -1,12 +1,19 @@
 package routers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+// 中间件
+func demoMiddlewareGet(content *gin.Context) {
+	fmt.Println("middlewareGet")
+	content.Next()
+}
+
 func DemoRouter(r *gin.Engine) {
-	g1 := r.Group("/group1/")
+	g1 := r.Group("/group1/", demoMiddlewareGet)
 	{
 		g1.GET(":uid", func(context *gin.Context) {
 			//va := context.Params.ByName("uid")
