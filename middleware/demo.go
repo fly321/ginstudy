c424ae37f00dc971853f30120476faa5f46686a2
fly3210 <15267840622@163.com>
2022-08-22 00:02:56 +0800

    中间件传值

--- middleware/demo.go
+++ middleware/demo.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 type Middlewares struct {
@@ -12,5 +13,14 @@ type Middlewares struct {
 func (con Middlewares) InitMd(content *gin.Context) {
 	fmt.Println("middlewareGet")
 	content.Set("name", "fly321")
+
+	// 必须使用copy对象 协程
+	cpc := content.Copy()
+
+	// 记录日志
+	go func() {
+		time.Sleep(time.Second * 2)
+		fmt.Println("Deno ! in middleware" + cpc.Request.URL.Path)
+	}()
 	content.Next()
 }
