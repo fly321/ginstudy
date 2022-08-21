1044f6e624bb371cdc097e120eebbbce5cc91b3b
fly3210 <15267840622@163.com>
2022-08-21 23:53:55 +0800

    中间件传值

--- routers/demo.go
+++ routers/demo.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"fmt"
 	"ginstudy/middleware"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -12,6 +13,7 @@ func DemoRouter(r *gin.Engine) {
 		g1.GET(":uid", func(context *gin.Context) {
 			//va := context.Params.ByName("uid")
 			va := context.Param("uid")
+			fmt.Println(context.Get("name"))
 			context.JSON(http.StatusOK, gin.H{
 				"uid": va,
 			})
