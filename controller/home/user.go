35fd826860d1a43e7fd0029e587f64feae98e5f8
fly3210 <15267840622@163.com>
2022-08-24 22:58:04 +0800

    修改数据2

--- controller/home/user.go
+++ controller/home/user.go
@@ -18,18 +18,18 @@ func (con UserController) IndexView(context *gin.Context) {
 }
 
 func (con UserController) IndexSelect(context *gin.Context) {
-	userList := []modules.User{}
+	var userList []modules.User
 	modules.DB.Find(&userList)
 	context.JSON(http.StatusOK, userList)
 }
 
 func (con UserController) IndexSelectAgeGt20(context *gin.Context) {
-	userList := []modules.User{}
+	var userList []modules.User
 	modules.DB.Where("age>20").Find(&userList)
 	context.JSON(http.StatusOK, userList)
 }
 
-func (con UserController) CreateData(context *gin.Context) {
+func (con UserController) CreateData(*gin.Context) {
 	user := modules.User{
 		Age:      33,
 		Username: "老吴",
@@ -39,8 +39,10 @@ func (con UserController) CreateData(context *gin.Context) {
 	modules.DB.Create(&user)
 }
 
-// 修改数据
-func (con UserController) UpdateData(context *gin.Context) {
+// UpdateData 修改数据
+//
+//goland:noinspection ALL
+func (con UserController) UpdateData(*gin.Context) {
 	/*user := modules.User{Id: 3}
 	modules.DB.Find(&user)
 	user.Username = "吴老菊"
